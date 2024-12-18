d7844e25caf83332bc94016716e48e7b9f2ac148
Trest40 <147926078+Trest40@users.noreply.github.com>
2024-12-18 20:55:38 +0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js"; // Обновлено до 9.22.1
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js"; // Обновлено до 9.22.1
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js"; // Обновлено до 9.22.1
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCGEvIce5EF1SFvKXa8xh0gqoCqQca26ag",
-    authDomain: "opros-klassa-8b.firebaseapp.com",
-    databaseURL: "https://opros-klassa-8b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "opros-klassa-8b",
-    storageBucket: "opros-klassa-8b.appspot.com",
-    messagingSenderId: "676010460004",
-    appId: "1:676010460004:web:6c3ec22f1066299898f071"
+  apiKey: "AIzaSyCGEvIce5EF1SFvKXa8xh0gqoCqQca26ag",
+  authDomain: "opros-klassa-8b.firebaseapp.com",
+  databaseURL: "https://opros-klassa-8b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "opros-klassa-8b",
+  storageBucket: "opros-klassa-8b.appspot.com", // Исправлено на opros-klassa-8b.appspot.com
+  messagingSenderId: "676010460004",
+  appId: "1:676010460004:web:6c3ec22f1066299898f071",
+  measurementId: "G-XW8RETP7CS"
 };
 
 // Initialize Firebase
@@ -18,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-export { auth, database };
+export { auth, database, GoogleAuthProvider }; // Добавлен экспорт GoogleAuthProvider
