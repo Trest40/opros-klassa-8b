6a75992383ff8cbf1fabd5ccf5d5c4c0bee454e0
Trest40 <147926078+Trest40@users.noreply.github.com>
2024-12-20 17:40:54 +0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,122 +1,135 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const signInButton = document.getElementById('sign-in-button');
-    const signOutButton = document.getElementById('sign-out-button');
-    const userInfo = document.getElementById('user-info');
-    const userNameElement = document.getElementById('user-name');
-    const votingForm = document.getElementById('voting-form');
-    const messageDiv = document.getElementById('message');
-    const voteButton = document.querySelector('.vote-button');
-    const nominationsContainer = document.getElementById('nominations-container');
+  const signInButton = document.getElementById('sign-in-button');
+  const signOutButton = document.getElementById('sign-out-button');
+  const userInfo = document.getElementById('user-info');
+  const userNameElement = document.getElementById('user-name');
+  const votingForm = document.getElementById('voting-form');
+  const voteButton = document.querySelector('.vote-button');
 
-    // Массив номинаций
-    const nominations = [
-        { id: 'golden_ball', title: 'Золотой мяч', options: ['Степа', 'Мирас', 'Алижан', 'Аднан'] },
-        { id: 'art_boss', title: 'Босс художки', options: ['Алиса', 'Вероника', 'Маша', 'Малика'] },
-        { id: 'late_boss', title: 'Босс опозданий', options: ['Даниил', 'Асель', 'Ян', 'Руслан'] },
-        { id: 'strong_men', title: 'Тестостероновые мужики', options: ['Артур', 'Ян', 'Костя', 'Коля'] },
-        { id: 'math_genius', title: 'Алгебраические гении', options: ['Аднан', 'Алижан', 'Акниет', 'Коля'] },
-        { id: 'lunch_lovers', title: 'Война войной, а обед по расписанию', options: ['Руслан', 'Алина Р.', 'Степа', 'Шерхан'] },
-        { id: 'anti_sport', title: 'Анти-спортсмен', options: ['Шерхан', 'Меседа', 'Алина Т.'] },
-        { id: 'it_geek', title: 'Айтишник', options: ['Костя', 'Артур', 'Коля', 'Асылбек'] },
-        { id: 'meloman', title: 'Меломан', options: ['Эвелина', 'Коля', 'Ангелина', 'Камиль'] },
-        { id: 'loudest', title: 'Самый громкий', options: ['Степа', 'Игорь', 'Ян', 'Руслан'] },
-    ];
+  function checkAuthentication() {
+    const userName = localStorage.getItem('userName');
 
-    // Генерация HTML для номинаций
-    nominations.forEach(nomination => {
-        const section = document.createElement('section');
-        section.className = 'nomination';
-        section.id = nomination.id;
+    if (userName) {
+      signInButton.style.display = 'none';
+      userNameElement.textContent = userName;
+      userInfo.style.display = 'flex';
+      voteButton.disabled = false;
+    } else {
+      voteButton.disabled = true;
+      signInButton.style.display = 'block';
+      userInfo.style.display = 'none';
+    }
+  }
 
-        const title = document.createElement('h2');
-        title.textContent = nomination.title;
+  function handleCredentialResponse(response) {
+    const responsePayload = jwt_decode(response.credential);
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
 
-        section.appendChild(title);
+    localStorage.setItem('userName', responsePayload.name);
+    localStorage.setItem('userEmail', responsePayload.email);
+    checkAuthentication();
 
-        nomination.options.forEach(option => {
-            const label = document.createElement('label');
-            const input = document.createElement('input');
+    // Отправляем форму после успешной авторизации
+    submitForm();
+  }
 
-            input.type = 'radio';
-            input.name = nomination.id;
-            input.value = option;
-
-            label.appendChild(input);
-            label.append(option);
-            section.appendChild(label);
+  window.onload = function() {
+    const checkGoogleApi = () => {
+      if (typeof google !== 'undefined' && google.accounts && google.accounts.id) {
+        google.accounts.id.initialize({
+          client_id: '847429882483-05f9mev63nq15t1ccilrjbnb27vrem42.apps.googleusercontent.com',
+          callback: handleCredentialResponse,
         });
-
-        nominationsContainer.appendChild(section);
-    });
-
-    // Функции аутентификации
-    function checkAuthentication() {
-        const userName = localStorage.getItem('userName');
-
-        if (userName) {
-            signInButton.style.display = 'none';
-            userNameElement.textContent = userName;
-            userInfo.style.display = 'flex';
-            voteButton.disabled = false;
-        } else {
-            voteButton.disabled = true;
-            signInButton.style.display = 'block';
-            userInfo.style.display = 'none';
-        }
+        google.accounts.id.renderButton(
+          signInButton,
+          { theme: "outline", size: "large" }
+        );
+      } else {
+        console.error("Google Identity Services library is not loaded.");
+        setTimeout(checkGoogleApi, 100); // Check again after 100ms
+      }
     }
+    checkGoogleApi();
+    checkAuthentication();
+  }
 
-    function handleCredentialResponse(response) {
-        const responsePayload = jwt_decode(response.credential);
+  signOutButton.addEventListener('click', function() {
+      localStorage.clear();
+      checkAuthentication();
 
-        localStorage.setItem('userName', responsePayload.name);
-        checkAuthentication();
+      voteButton.disabled = false;
+      voteButton.textContent = 'Голосовать';
+  });
+
+  // Обработчик на кнопку голосования
+  voteButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (localStorage.getItem('userName')) {
+      // Пользователь авторизован, отправляем форму
+      submitForm();
+    } else {
+      // Пользователь не авторизован, показываем сообщение и инициируем вход
+      alert('Пожалуйста, войдите в аккаунт, чтобы проголосовать.');
+      document.getElementById('auth-container').scrollIntoView({ behavior: 'smooth' });
+
+      // Показываем кнопку входа
+      signInButton.click();
     }
+  });
 
-    window.onload = function () {
-        if (typeof google !== 'undefined' && google.accounts && google.accounts.id) {
-            google.accounts.id.initialize({
-                client_id: '847429882483-05f9mev63nq15t1ccilrjbnb27vrem42.apps.googleusercontent.com',
-                callback: handleCredentialResponse,
-            });
-            google.accounts.id.renderButton(signInButton, { theme: 'outline', size: 'large' });
-        }
-        checkAuthentication();
-    };
-
-    signOutButton.addEventListener('click', function () {
-        localStorage.clear();
-        checkAuthentication();
-    });
-
-    // Голосование
-    voteButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        if (!localStorage.getItem('userName')) {
-            messageDiv.textContent = 'Пожалуйста, войдите в аккаунт, чтобы проголосовать.';
-            messageDiv.style.display = 'block';
-            setTimeout(() => { messageDiv.style.display = 'none'; }, 5000);
-            return;
-        }
-
+  // Функция отправки формы
+  function submitForm() {
         voteButton.textContent = 'Отправка...';
         voteButton.disabled = true;
 
-        const formData = new FormData(votingForm);
+        // Сбор данных формы вручную
+        const formData = {};
+        for (const element of votingForm.elements) {
+          if (element.name && element.type !== 'submit') {
+            if (element.type === 'radio' && element.checked) {
+              formData[element.name] = element.value;
+            } else if (element.type !== 'radio') {
+              formData[element.name] = element.value;
+            }
+          }
+        }
+        formData['email'] = localStorage.getItem('userEmail');
+
+        console.log('Отправляемые данные:', JSON.stringify(formData));
+
         fetch(votingForm.action, {
-            method: 'POST',
-            body: formData,
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
         })
-            .then(response => response.text())
-            .then(() => {
-                messageDiv.textContent = 'Спасибо за ваш голос!';
-                messageDiv.style.display = 'block';
-                votingForm.reset();
-            })
-            .catch(error => console.error(error))
-            .finally(() => {
-                voteButton.textContent = 'Голосовать';
-                voteButton.disabled = false;
-            });
-    });
+          .then(response => {
+            if (response.ok) {
+              return response.text();
+            } else {
+              throw new Error('Произошла ошибка при отправке формы.');
+            }
+          })
+          .then(responseText => {
+            console.log(responseText);
+
+            alert('Спасибо за ваш голос!');
+
+            votingForm.reset();
+          })
+          .catch(error => {
+            console.error('Ошибка:', error);
+            alert(error.message);
+          })
+          .finally(() => {
+            voteButton.textContent = 'Голосовать';
+            voteButton.disabled = false;
+          });
+  }
 });
