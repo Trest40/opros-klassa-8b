01f39921ca1d02f92a7b2b09c12de545bb59851b
Trest40 <147926078+Trest40@users.noreply.github.com>
2024-12-20 22:42:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initializeGoogleSignIn() {
   return new Promise((resolve) => {
     const script = document.createElement('script');
-    script.src = "https://accounts.google.com/gsi/client";
+    script.src = 'https://accounts.google.com/gsi/client';
     script.onload = () => {
       if (google && google.accounts && google.accounts.id) {
         resolve();
@@ -13,78 +13,104 @@ function initializeGoogleSignIn() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
-  const signInButton = document.getElementById("sign-in-button");
-  const signOutButton = document.getElementById("sign-out-button");
-  const userInfo = document.getElementById("user-info");
-  const userNameElement = document.getElementById("user-name");
-  const votingForm = document.getElementById("voting-form");
-  const voteButton = document.querySelector(".vote-button");
+document.addEventListener('DOMContentLoaded', async function () {
+  const signInButton = document.getElementById('sign-in-button');
+  const signOutButton = document.getElementById('sign-out-button');
+  const userInfo = document.getElementById('user-info');
+  const userNameElement = document.getElementById('user-name');
+  const votingForm = document.getElementById('voting-form');
+  const voteButton = document.querySelector('.vote-button');
+
+  // Добавляем класс animate-fade-in для анимации появления
+  const elementsToAnimate = document.querySelectorAll('header, .nomination, .vote-button, footer');
+  elementsToAnimate.forEach(element => {
+    element.classList.add('animate-fade-in');
+  });
 
   function checkAuthentication() {
-    const userName = localStorage.getItem("userName");
+    const userName = localStorage.getItem('userName');
 
     if (userName) {
-      signInButton.style.display = "none";
+      signInButton.style.display = 'none';
       userNameElement.textContent = userName;
-      userInfo.style.display = "flex";
+      userInfo.style.display = 'flex';
       voteButton.disabled = false;
     } else {
       voteButton.disabled = true;
-      signInButton.style.display = "block";
-      userInfo.style.display = "none";
+      signInButton.style.display = 'block';
+      userInfo.style.display = 'none';
     }
   }
 
   function handleCredentialResponse(response) {
     const responsePayload = jwt_decode(response.credential);
     console.log("ID: " + responsePayload.sub);
-    console.log("Full Name: " + responsePayload.name);
+    console.log('Full Name: " + responsePayload.name);
     console.log("Image URL: " + responsePayload.picture);
     console.log("Email: " + responsePayload.email);
 
-    localStorage.setItem("userName", responsePayload.name);
-    localStorage.setItem("userEmail", responsePayload.email);
+    localStorage.setItem('userName', responsePayload.name);
+    localStorage.setItem('userEmail', responsePayload.email);
     checkAuthentication();
   }
 
   try {
     await initializeGoogleSignIn();
     google.accounts.id.initialize({
-      client_id:
-        "847429882483-05f9mev63nq15t1ccilrjbnb27vrem42.apps.googleusercontent.com",
-      callback: handleCredentialResponse,
+      client_id: '847429882483-05f9mev63nq15t1ccilrjbnb27vrem42.apps.googleusercontent.com',
+      callback: handleCredentialResponse
     });
-    google.accounts.id.renderButton(signInButton, {
-      theme: "outline",
-      size: "large",
-    });
+    google.accounts.id.renderButton(
+      signInButton,
+      { theme: "outline", size: "large" }
+    );
     checkAuthentication();
   } catch (error) {
     console.error("Error initializing Google Sign-In:", error);
   }
 
-  signOutButton.addEventListener("click", function () {
+  signOutButton.addEventListener('click', function () {
     localStorage.clear();
     checkAuthentication();
     voteButton.disabled = false;
-    voteButton.textContent = "Голосовать";
+    voteButton.textContent = 'Голосовать';
   });
 
-  voteButton.addEventListener("click", function (event) {
+  voteButton.addEventListener('click', function (event) {
     event.preventDefault();
 
-    if (localStorage.getItem("userName")) {
+    if (localStorage.getItem('userName')) {
       submitForm();
     } else {
-      alert("Пожалуйста, войдите в аккаунт, чтобы проголосовать.");
-      document
-        .getElementById("auth-container")
-        .scrollIntoView({ behavior: "smooth" });
+      alert('Пожалуйста, войдите в аккаунт, чтобы проголосовать.');
+      document.getElementById('auth-container').scrollIntoView({ behavior: 'smooth' });
       signInButton.click();
     }
   });
 
   function submitForm() {
-    voteButton.textContent = "Отправка...";
-    voteButton.disabled = true
+    voteButton.textContent = 'Отправка...';
+    voteButton.disabled = true;
+    let formSubmitted = false;
+
+    const formData = {};
+    for (const element of votingForm.elements) {
+      if (element.name && element.type !== 'submit') {
+        if (element.type === 'radio' && element.checked) {
+          formData[element.name] = element.value;
+        } else if (element.type !== 'radio') {
+          formData[element.name] = element.value;
+        }
+      }
+    }
+    formData['email'] = localStorage.getItem('userEmail');
+
+    console.log('Отправляемые данные:', JSON.stringify(formData));
+
+    fetch(votingForm.action, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
