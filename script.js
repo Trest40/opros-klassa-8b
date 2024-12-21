1b33990564d89f349abe2409bc629347e20b73b2
Trest40 <147926078+Trest40@users.noreply.github.com>
2024-12-21 18:57:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Initialize Google API
   function initializeGoogleSignIn() {
-    console.log('initializeGoogleSignIn called'); // Логируем
     if (typeof google !== 'undefined' && google.accounts && google.accounts.id) {
-      console.log('Google API is defined. Initializing.'); // Логируем
       google.accounts.id.initialize({
         // client_id: googleClientId, //No need for client_id here
         callback: handleCredentialResponse,
@@ -23,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
         ux_mode: 'popup', // Use popup mode for a cleaner UX
         itp_support: true, // Enable Intelligent Tracking Prevention support
       });
-      // google.accounts.id.prompt();
+      //  google.accounts.id.prompt();
     } else {
       console.error('Google API is not initialized.');
       showNotification(
@@ -45,8 +43,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function checkAuthentication() {
     const userName = localStorage.getItem('userName');
-    console.log('checkAuthentication called. userName:', userName); // Логируем
-
     if (userName) {
       authButtons.style.display = 'none';
       userInfo.style.display = 'flex';
@@ -60,29 +56,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  window.handleCredentialResponse = (response) => {
-    console.log('handleCredentialResponse called. response:', response); // Логируем
-
+  function handleCredentialResponse(response) {
     if (response && response.credential) {
       try {
         const responsePayload = jwt_decode(response.credential);
-        console.log('responsePayload', responsePayload);
+        console.log('ID: ' + responsePayload.sub);
+        console.log('Full Name: ' + responsePayload.name);
+        console.log('Image URL: ' + responsePayload.picture);
+        console.log('Email: ' + responsePayload.email);
 
         localStorage.setItem('userName', responsePayload.name);
         localStorage.setItem('userEmail', responsePayload.email);
-
-        // ПРОВЕРКА НА НАЛИЧИЕ ИМЕНИ В localstorage
-        if (localStorage.getItem('userName')) {
-          console.log('User is logged in. Updating UI.'); // Логируем
-          checkAuthentication();
-          showNotification('success', 'Вы успешно авторизовались!');
-        } else {
-          console.error('User name not found in local storage after login.');
-          showNotification(
-            'error',
-            'Ошибка авторизации. Имя пользователя не найдено.'
-          );
-        }
+        checkAuthentication();
+        showNotification('success', 'Вы успешно авторизовались!');
       } catch (error) {
         console.error('Error decoding or storing credentials:', error);
         showNotification(
@@ -90,14 +76,8 @@ document.addEventListener('DOMContentLoaded', function () {
           'Ошибка авторизации. Пожалуйста, попробуйте еще раз.'
         );
       }
-    } else {
-      console.error('Credential response is invalid or missing.');
-      showNotification(
-        'error',
-        'Ошибка авторизации. Пожалуйста, попробуйте еще раз.'
-      );
     }
-  };
+  }
 
   signOutButton.addEventListener('click', function () {
     localStorage.clear();
